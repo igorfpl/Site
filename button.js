d27a742a4d90c5c36e79374650e786557d63ed26
igorfpl <61692834+igorfpl@users.noreply.github.com>
2020-03-13 10:33:47 -0300

    Add files via upload

---
+++ button.js
@@ -0,0 +1,5 @@
+function botao(){
+    document.getElementById('demo').innerHTML='Toasty';
+    document.getElementById('demo2').innerHTML = 'hello world';
+    document.getElementById('demo3').innerHTML = 'Parece que vc clickou no easter';
+}
